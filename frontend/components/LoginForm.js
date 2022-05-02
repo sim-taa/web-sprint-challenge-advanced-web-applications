29ea88cfd0b110195ce680c068e388f0e7d7fd83
Simone Taarnskov <simone.taarnskov@gmail.com>
2022-05-02 13:41:38 -0700

    finished LoginForm. One test now passing. Still has errors.

--- frontend/components/LoginForm.js
+++ frontend/components/LoginForm.js
@@ -20,12 +20,17 @@ export default function LoginForm(props) {
     login(values)
   }
 
-  // const isDisabled = () => {
-  //   // ✨ implement
-  //   // Trimmed username must be >= 3, and
-  //   // trimmed password must be >= 8 for
-  //   // the button to become enabled
-  // }
+const isDisabled = () => {
+  // ✨ implement
+  // Trimmed username must be >= 3, and
+  // trimmed password must be >= 8 for
+  // the button to become enabled
+  if (values.username.trim().length >= 3 && values.password.trim().length >= 8) {
+    return false;
+  }else{
+    return true;
+  }
+}
 
   return (
     <form id="loginForm" onSubmit={onSubmit}>
